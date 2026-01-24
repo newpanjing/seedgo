0caf590b9520cd5d64b28476e45d860eaebba19b
panjing <newpanjing@163.com>
2026-01-24 16:22:59 +0800

    chore: 忽略并移除系统生成的.DS_Store文件
    添加.DS_Store到.gitignore以避免未来提交，并从版本控制中移除已提交的文件。

---
+++ frontend/src/components/common/TableCellFormat.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { formatDate } from '@/utils/date'
+
+const props = defineProps({
+    value: {
+        //时间戳
+        type: [Number, String],
+        default: () => null
+    },
+    format: {
+        type: String,
+        default: 'YYYY-MM-DD HH:mm:ss'
+    }
+})
+
+const diaplay = computed(() => {
+    return formatDate(props.value, props.format)
+})
+</script>
+<template>
+    {{ diaplay }}
+</template>
