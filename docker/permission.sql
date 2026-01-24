960779c945433cad534ff5dbae0116a096fef946
panjing <newpanjing@163.com>
2026-01-24 18:11:50 +0800

    docs: 新增权限表初始化SQL文件

---
+++ docker/permission.sql
@@ -0,0 +1,23 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : local_smart
+ Source Server Type    : MySQL
+ Source Server Version : 80044 (8.0.44)
+ Source Host           : localhost:3306
+ Source Schema         : smart
+
+ Target Server Type    : MySQL
+ Target Server Version : 80044 (8.0.44)
+ File Encoding         : 65001
+
+ Date: 24/01/2026 18:00:04
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for permission
+-- ----------------------------
+DROP TABLE IF EXISTS `permission`;
